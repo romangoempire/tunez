bc23bd8a172ca58d23b5c156f834ea572f69873b
RomanGoEmpire <71276897+RomanGoEmpire@users.noreply.github.com>
2025-10-09 09:06:19 +0200

    add: register and signin in apis

--- lib/tunez/accounts.ex
+++ lib/tunez/accounts.ex
@@ -1,6 +1,40 @@
 defmodule Tunez.Accounts do
-  use Ash.Domain,
-    otp_app: :tunez
+  use Ash.Domain, otp_app: :tunez, extensions: [AshGraphql.Domain, AshJsonApi.Domain]
+
+  graphql do
+    queries do
+      get Tunez.Accounts.User, :sign_in_user, :sign_in_with_password do
+        identity false
+        type_name :user_with_token
+      end
+    end
+
+    mutations do
+      create Tunez.Accounts.User, :register_user, :register_with_password
+    end
+  end
+
+  json_api do
+    routes do
+      base_route "/users", Tunez.Accounts.User do
+        post :register_with_password do
+          route "/register"
+
+          metadata fn _subject, user, _request ->
+            %{token: user.__metadata__.token}
+          end
+        end
+
+        post :sign_in_with_password do
+          route "/signin"
+
+          metadata fn _subject, user, _request ->
+            %{token: user.__metadata__.token}
+          end
+        end
+      end
+    end
+  end
 
   resources do
     resource Tunez.Accounts.Token
--- lib/tunez/accounts/user.ex
+++ lib/tunez/accounts/user.ex
@@ -4,7 +4,7 @@ defmodule Tunez.Accounts.User do
     domain: Tunez.Accounts,
     data_layer: AshPostgres.DataLayer,
     authorizers: [Ash.Policy.Authorizer],
-    extensions: [AshAuthentication]
+    extensions: [AshGraphql.Resource, AshJsonApi.Resource, AshAuthentication]
 
   authentication do
     add_ons do
@@ -52,6 +52,14 @@ defmodule Tunez.Accounts.User do
         sender Tunez.Accounts.User.Senders.SendMagicLinkEmail
       end
     end
+  end
+
+  graphql do
+    type :user
+  end
+
+  json_api do
+    type "user"
   end
 
   postgres do
@@ -260,6 +268,10 @@ defmodule Tunez.Accounts.User do
 
   policies do
     bypass AshAuthentication.Checks.AshAuthenticationInteraction do
+      authorize_if always()
+    end
+
+    policy action([:register_with_password, :sign_in_with_password]) do
       authorize_if always()
     end
   end
